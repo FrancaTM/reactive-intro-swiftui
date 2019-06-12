b0e944d19e63a1039b1d122422ca98b798058fb3
FrancaTM <francatm@outlook.com>
2019-06-12 14:08:31 -0300

    sample finished

--- ReactiveIntro/ContentView.swift
+++ ReactiveIntro/ContentView.swift
@@ -8,14 +8,69 @@
 
 import SwiftUI
 
-struct ContentView : View {
+struct ContentView: View {
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var users = [String]()
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                VStack {
+                    VStack {
+                        Group {
+                            TextField($firstName, placeholder: Text("First name")).padding(12)
+                        }
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .shadow(radius: 5)
+
+                        Group {
+                            TextField($lastName, placeholder: Text("Last name")).padding(12)
+                        }
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .shadow(radius: 5)
+
+                        HStack {
+                            Button(action: {
+                                self.users.append("\(self.firstName) \(self.lastName)")
+                                self.firstName = ""
+                                self.lastName = ""
+                            }) {
+                                Group {
+                                    Text("Create user").color(Color.white).padding(12)
+                                }
+                                .background((firstName.count + lastName.count > 0) ? Color.blue : Color.gray)
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                                .shadow(radius: 5)
+                            }
+                            Button(action: {
+                                self.users.removeAll()
+                            }) {
+                                Text("Delete users")
+                            }
+                        }
+                    }.padding(12)
+                }.background(Color.gray)
+
+                List(users.identified(by: \.self)) {
+                    Text($0)
+                }
+            }.navigationBarTitle(Text("Credit card form"))
+                .navigationBarItems(leading: HStack {
+                    Text("First name:")
+                    Text(firstName).color(.red)
+                    Text("Last name:")
+                    Text(lastName).color(.red)
+
+                })
+        }
     }
 }
 
 #if DEBUG
-struct ContentView_Previews : PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
